15f193e2a077cfa452eeca70ab9e3a10241c3598
arthurnovaisdev <arthurnovaispeixoto@gmail.com>
2026-08-14 11:39:37 -0300

    Service layer, component registration

---
+++ src/main/java/com/arthurnovaisdev/web_services/services/UserService.java
@@ -0,0 +1,25 @@
+package com.arthurnovaisdev.web_services.services;
+
+import com.arthurnovaisdev.web_services.entities.User;
+import com.arthurnovaisdev.web_services.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}

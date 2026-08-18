782b327fcf165f9f6c6dc58a6c7c32183b913690
arthurnovaisdev <arthurnovaispeixoto@gmail.com>
2026-08-18 16:46:14 -0300

    Exception handling - findByID

--- src/main/java/com/arthurnovaisdev/web_services/services/UserService.java
+++ src/main/java/com/arthurnovaisdev/web_services/services/UserService.java
@@ -2,6 +2,7 @@ package com.arthurnovaisdev.web_services.services;
 
 import com.arthurnovaisdev.web_services.entities.User;
 import com.arthurnovaisdev.web_services.repositories.UserRepository;
+import com.arthurnovaisdev.web_services.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {

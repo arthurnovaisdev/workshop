fe8ad282d9b6f32c1594867f5b833c3de41075b5
arthurnovaisdev <arthurnovaispeixoto@gmail.com>
2026-08-19 11:23:32 -0300

    ExceptionHandler - delete

---
+++ src/main/java/com/arthurnovaisdev/web_services/services/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package com.arthurnovaisdev.web_services.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
